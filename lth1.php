06d6c8a8b08efbf08fd27957177dd7b02da9fe7a
DzakyFaishalariq <84628553+dzakyfaishalariq@users.noreply.github.com>
2022-02-26 21:33:57 +0700

    ini adalah komentar php

---
+++ lth1.php
@@ -0,0 +1,14 @@
+<?php
+// membuat komentar
+// komentar petama saya
+/* 
+komentar kedua saya 
+*/
+// maka hal ini akan saya mulai
+// untuk melakukan hal yang luar biasa 
+// mengunakan echo "ini adalah komentar";
+// print_r("ini adalah komentar");
+// untuk mendibug
+// var_dump("ini adalah komentar");
+
+echo "hallo saya adalah Dzaky fasihalariq";
