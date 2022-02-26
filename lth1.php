69b21694e480699861769c28b6c82f40cab3d863
DzakyFaishalariq <84628553+dzakyfaishalariq@users.noreply.github.com>
2022-02-26 22:03:01 +0700

    penggabungan string

--- lth1.php
+++ lth1.php
@@ -11,6 +11,35 @@ komentar kedua saya
 // untuk mendibug
 // var_dump("ini adalah komentar");
 
-echo "hallo saya adalah Dzaky fasihalariq <br>";
-print_r('hallo saya adalah Dzaky fasihalariq <br>');
-var_dump('hallo saya adalah Dzaky fasihalariq <br>');
+// echo "hallo saya adalah Dzaky fasihalariq <br>";
+// print_r('hallo saya adalah Dzaky fasihalariq <br>');
+// var_dump('hallo saya adalah Dzaky fasihalariq <br>');
+$nama = "Dzaky Fasihalariq";
+$NPM = "G1A020003";
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar php 1</title>
+</head>
+
+<body>
+    <?php
+    for ($i = 0; $i < 4; $i++) {
+    ?>
+        <h1>Hallo saya adalah <?php echo $nama ?></h1>
+        <hr>
+    <?php
+    }
+    ?>
+    <?php
+    // membuat penggabungan string
+    echo $nama . " " . $NPM;
+    ?>
+</body>
+
+</html>
